79ef26ca617045cce2d0dee1a0b06861005454e5
Pol Milian <polcius@gmail.com>
2018-09-28 15:41:22 +0930

    globalStyle instead of injecStyle, cursor and font OK

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,42 @@ import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import { StaticQuery, graphql } from "gatsby";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import AvantBold from "../fonts/ITCAvantGardePro-Bold.woff";
+import PointerWhite from "../images/icons/white-default.png";
 
 import Header from "./header";
-import "./layout.css";
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  /* other styles */
+  :root {
+    // Colors
+    --black: #111111;
+    --pink: #ff71b0;
+    --cyan: #00ffff;
+    --green: #41fa70;
+    --yellow: #fff36d;
+    // Cursors
+    --pointerwhite: url(${PointerWhite}) 0 0, auto;
+  }
+  
+  @font-face {
+    font-family: "AvantGardeBold";
+    src: url(${AvantBold}) format("woff");
+  }
+
+  html {
+    cursor: var(--pointerwhite);
+  }
+
+  body {
+    background-color: var(--black);
+    font-family: "AvantGardeBold";
+    
+  }
+`;
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -20,6 +52,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
+        <GlobalStyle />
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
@@ -29,17 +62,8 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: "0 auto",
-            maxWidth: 960,
-            padding: "0px 1.0875rem 1.45rem",
-            paddingTop: 0
-          }}
-        >
-          {children}
-        </div>
+        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+        <div>{children}</div>
       </>
     )}
   />
