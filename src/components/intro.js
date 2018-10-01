21a35cb0b34897953023a994305a07e0a5a32bb7
Pol Milian <polcius@gmail.com>
2018-10-01 17:13:29 +0930

    added intro component for main text

---
+++ src/components/intro.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "gatsby";
+import styled from "styled-components";
+
+const IntroWrapper = styled.div`
+  color: white;
+  margin: 0 100px;
+`;
+
+const IntroText = styled.p`
+  font-size: 4rem;
+`;
+
+const Intro = () => (
+  <IntroWrapper>
+    <p>AAA</p>
+  </IntroWrapper>
+);
+
+export default Intro;
