be38e833da363080d651df20cdc0d99513d4b728
Pol Milian <polcius@gmail.com>
2018-10-02 17:42:31 +0930

    project component draft

---
+++ src/components/projects.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "gatsby";
+import styled from "styled-components";
+
+// Grid de 1 full x 2
+// El de full es veu a 80-90vh
+
+/*
+Projects Schema:
+
+- Fecha
+- Introduccion
+- Creditos
+- Imagenes
+- Videos
+- Proyectos relacionados
+
+
+*/
+
+// const Projects = () => ()
+
+// export default Projects;
