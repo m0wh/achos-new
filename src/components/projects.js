5c68ee412ba65fe04ff69637b0e4c2404f756e4c
Pol Milian <polcius@gmail.com>
2018-10-03 17:47:43 +0930

    grid tests

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
 
+import MarianoPic from "../images/mariano.jpg";
+
 // Grid de 1 full x 2
 // El de full es veu a 80-90vh
 
@@ -15,9 +17,40 @@ Projects Schema:
 - Videos
 - Proyectos relacionados
 
-
 */
 
-// const Projects = () => ()
+const projectNumber = 1;
+const ProjectsGrid = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto;
+`;
 
-// export default Projects;
+const BigProject = styled.div`
+  border: 1px solid red;
+  background: url(${MarianoPic});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 80vh;
+  grid-column: span 2;
+`;
+
+const SmallProject = styled.div`
+  border: 1px solid red;
+  background: url(${MarianoPic});
+  background-color: yellow;
+  height: 40vh;
+`;
+
+const Projects = () => (
+  <ProjectsGrid>
+    <BigProject />
+    <SmallProject />
+    <SmallProject />
+    <SmallProject />
+    <SmallProject />
+  </ProjectsGrid>
+);
+
+export default Projects;
