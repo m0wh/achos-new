af82a4b88f91568d3640e428c08fa3c2345682c9
Pol Milian <polcius@gmail.com>
2018-10-04 17:21:29 +0930

    extracted header component link to project page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,33 +3,20 @@ import { Link } from "gatsby";
 import styled from "styled-components";
 
 import Layout from "../components/layout";
-import Navbar from "../components/navbar";
-import Logo from "../components/logo";
+import Header from "../components/header";
 import Intro from "../components/intro";
-import Projects from "../components/projects"
+import Projects from "../components/projects";
 import Contact from "../components/contact";
 import LuckyDay from "../components/luckyday";
 
-const Header = styled.header`
-  max-width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: baseline;
-  color: white;
-  font-size: 3rem;
-  margin: 2.625rem 3.1875rem 0 3.5625rem;
-`;
-
 const IndexPage = () => (
   <Layout>
-    <Header>
-      <Logo />
-      <Navbar />
-    </Header>
+    <Header />
     <Intro />
     <Projects />
     <Contact />
     <LuckyDay />
+    <Link to="/project">Link</Link>
   </Layout>
 );
 
