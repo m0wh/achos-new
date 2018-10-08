48522bbaf63c4d76e2edb735e0015f518cd0436a
Pol Milian <polcius@gmail.com>
2018-10-08 17:40:27 +1030

    5sec scrolling test

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,15 @@
 import React from "react";
-import { Link } from "gatsby";
+// import { Link } from "gatsby";
 import styled from "styled-components";
+import {
+  Link,
+  DirectLink,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller
+} from "react-scroll";
 
 const NavWrapper = styled.nav`
   z-index: 1000;
@@ -41,28 +50,56 @@ const StyledLink = styled(Link)`
 //     onMouseOver change states?
 // }
 
-const Navbar = () => (
-  <NavWrapper>
-    <List>
-      {/* onClick scrolls through page */}
-      <ListItem color="var(--yellow)">
-        <StyledLink to="/">5 Second Tour</StyledLink>
-      </ListItem>
-      {/* onClick scrolls down to beginning of projects */}
-      <ListItem color="var(--cyan)">
-        <StyledLink to="/">work</StyledLink>
-      </ListItem>
-      {/* StyledLink to About */}
-      <ListItem color="var(--green)">
-        <StyledLink to="/">about</StyledLink>
-      </ListItem>
-      {/* StyledLink to Espai */}
-      <ListItem color="var(--pink)">
-        <StyledLink to="#Contact">contact</StyledLink>
-      </ListItem>
-      <ListItem color="var(--darkgrey)">eng</ListItem>
-    </List>
-  </NavWrapper>
-);
+class Navbar extends React.Component {
+  componentDidMount() {
+    Events.scrollEvent.register("begin", function() {
+      console.log("begin", arguments);
+    });
+
+    Events.scrollEvent.register("end", function() {
+      console.log("end", arguments);
+    });
+  }
+
+  componentWillUnmount() {
+    Events.scrollEvent.remove("begin");
+    Events.scrollEvent.remove("end");
+  }
+
+  render() {
+    return (
+      <NavWrapper>
+        <List>
+          {/* onClick scrolls through page */}
+          <ListItem color="var(--yellow)">
+            <StyledLink
+              onClick={() =>
+                scroll.scrollToBottom({
+                  duration: 5000,
+                  smooth: true
+                })
+              }
+            >
+              5 Second Tour
+            </StyledLink>
+          </ListItem>
+          {/* onClick scrolls down to beginning of projects */}
+          <ListItem color="var(--cyan)">
+            <StyledLink to="/">work</StyledLink>
+          </ListItem>
+          {/* StyledLink to About */}
+          <ListItem color="var(--green)">
+            <StyledLink to="/">about</StyledLink>
+          </ListItem>
+          {/* StyledLink to Espai */}
+          <ListItem color="var(--pink)">
+            <StyledLink to="#Contact">contact</StyledLink>
+          </ListItem>
+          <ListItem color="var(--darkgrey)">eng</ListItem>
+        </List>
+      </NavWrapper>
+    );
+  }
+}
 
 export default Navbar;
