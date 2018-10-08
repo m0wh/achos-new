7e4a9944f1b642494760941087f2eb8945976fe2
Pol Milian <polcius@gmail.com>
2018-10-08 17:22:04 +1030

    renamed projects to work

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import Layout from "../components/layout";
 import Header from "../components/header";
 import Intro from "../components/intro";
-import Projects from "../components/projects";
+import Work from "../components/work";
 import Contact from "../components/contact";
 import LuckyDay from "../components/luckyday";
 
@@ -13,10 +13,9 @@ const IndexPage = () => (
   <Layout>
     <Header />
     <Intro />
-    <Projects />
+    <Work />
     <Contact />
     <LuckyDay />
-    <Link to="/project">Link</Link>
   </Layout>
 );
 
