c20dbe64dcda53c71a16ebce37e25cee0cee448f
Pol Milian <polcius@gmail.com>
2018-10-12 14:06:14 +1030

    removed unused imports added contact links

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,13 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
-import {
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
-  scroller
-} from "react-scroll";
+import { Element } from "react-scroll";
 
 import FacebookIcon from "../images/icons/facebook.svg";
 import TinderIcon from "../images/icons/tinder.svg";
@@ -53,30 +47,78 @@ const Contact = () => (
     <ContactWrapper>
       <ContactUs>
         <li>Contact</li>
-        <ListItem color="var(--pink)">hi@achos.es</ListItem>
-        <ListItem color="var(--cyan)">(+34) 935 016 671</ListItem>
-        <ListItem color="var(--green)">(+34) 630 991 990</ListItem>
+        <ListItem color="var(--pink)">
+          <a
+            href="mailto:hi@achos.es?Subject=Shut up and take my money!"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hi@achos.es
+          </a>
+        </ListItem>
+        <ListItem color="var(--cyan)">
+          <a href="tel:+34935016671" target="_blank" rel="noopener noreferrer">
+            (+34) 935 016 671
+          </a>
+        </ListItem>
+        <ListItem color="var(--green)">
+          <a href="tel:+34630991990" target="_blank" rel="noopener noreferrer">
+            (+34) 630 991 990
+          </a>
+        </ListItem>
         <ListItem color="var(--yellow)">
-          Vigatans 11, Local 2, 08003 Barcelona
+          <a
+            href="https://goo.gl/maps/p4svHoawcbq"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <address>Vigatans 11, Local 2, 08003 Barcelona</address>
+          </a>
         </ListItem>
       </ContactUs>
       <FollowUs>
         <li>Follow Us!</li>
+
         <ListItem color="var(--pink)">
-          <SocialIcon src={TinderIcon} />
-          Tinder
+          <a
+            href="http://www.gotinder.com/@achos"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SocialIcon src={TinderIcon} />
+            Tinder
+          </a>
         </ListItem>
+
         <ListItem color="var(--cyan)">
-          <SocialIcon src={FacebookIcon} />
-          Facebook
+          <a
+            href="http://www.facebook.com/achosagency"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SocialIcon src={FacebookIcon} />
+            Facebook
+          </a>
         </ListItem>
         <ListItem color="var(--green)">
-          <SocialIcon src={YoutubeIcon} />
-          Instagram
+          <a
+            href="http://www.instagram.com/achosagency"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SocialIcon src={YoutubeIcon} />
+            Instagram
+          </a>
         </ListItem>
         <ListItem color="var(--yellow)">
-          <SocialIcon src={TwitterIcon} />
-          Twitter
+          <a
+            href="https://twitter.com/achosagency"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SocialIcon src={TwitterIcon} />
+            Twitter
+          </a>
         </ListItem>
       </FollowUs>
     </ContactWrapper>
