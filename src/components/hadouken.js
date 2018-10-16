0f31cdb050123714fc289f5c10b1aa72f17c0b88
Pol Milian <polcius@gmail.com>
2018-10-16 12:08:34 +1030

    need to work on code

---
+++ src/components/hadouken.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import Konami from 'react-konami-code'
+// import HadoukenSound from "../images/hadouken.mp3"
+
+// export default class Hadouken extends React.Component {
+//   easterEgg = () => {
+//     this.sound.play();
+
+//   }
+
+//   render = () => (
+//     <>
+//     <Konami action={this.easterEgg} code={[40, 39, 65]} resetDelay={0}>
+//       <audio preload="auto" ref={(test) => { this.sound = test; }}><source src={HadoukenSound} type="audio/mpeg"></source></audio>
+//     </Konami>
+//     </>
+//   )
+// }
