84ef99dc2849e404a19cc91c0be70742e85a2c7a
Pol Milian <polcius@gmail.com>
2018-10-18 13:48:58 +1030

    test

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,22 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
-
-import SoundGif from "../components/soundgif";
-
-import Sound from "../images/barcelona.mp3";
-import GIF from "../images/gifs/5sectour.gif";
-
-import Sound2 from "../images/hadouken.mp3";
+import Example from "../components/mobilemenu";
 
 const YourComponent = () => (
   <div>
-    <SoundGif sound={Sound} gif={GIF}>
-      POLOS
-    </SoundGif>
-    <SoundGif sound={Sound2} gif={GIF}>
-      LOP
-    </SoundGif>
+    <Example />
   </div>
 );
 
