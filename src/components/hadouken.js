0f7fd3e17a111282f4fc909d6e47a6adca6d621d
Pol Milian <polcius@gmail.com>
2018-10-19 15:50:17 +1030

    wrote main code

--- src/components/hadouken.js
+++ src/components/hadouken.js
@@ -1,32 +1,44 @@
 // import React from 'react'
-// import Konami from 'react-konami-code'
 // import HadoukenSound from "../images/hadouken.mp3"
 
+
+// const pressed = [];
+// const secretCode = "wesbos";
+
 // export default class Hadouken extends React.Component {
-//   easterEgg = () => {
+  
+
+//   componentDidMount() {
+//     window.addEventListener("keyup", e => {
+//       console.log(e.key);
+//       pressed.push(e.key);
+//       pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+//       if (pressed.join("").includes(secretCode)) {
+//         this.playSound();
+        
+//       }
+//       console.log(pressed);
+//     });
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener("keyup", e);
+//   }
+//   playSound = () => {
 //     this.sound.play();
 
 //   }
 
 //   render = () => (
-//     <>
-//     <Konami action={this.easterEgg} code={[40, 39, 65]} resetDelay={0}>
+    
+    
+//     <div>
 //       <audio preload="auto" ref={(test) => { this.sound = test; }}><source src={HadoukenSound} type="audio/mpeg"></source></audio>
-//     </Konami>
-//     </>
+//     </div>
+    
 //   )
-// // }
+// }
 
-// const pressed = [];
-// const secretCode = "wesbos";
 
-// window.addEventListener("keyup", e => {
-//   console.log(e.key);
-//   pressed.push(e.key);
-//   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-//   if (pressed.join("").includes(secretCode)) {
-//     console.log("DING DING!");
-//     cornify_add();
-//   }
-//   console.log(pressed);
-// });
+
+
