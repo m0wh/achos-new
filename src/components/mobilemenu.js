3bb2361f9fdbf25a926cc3168c21a1ce1f6ef1a3
Pol Milian <polcius@gmail.com>
2018-10-19 13:28:58 +1030

    reverted to using react-burger-menu

--- src/components/mobilemenu.js
+++ src/components/mobilemenu.js
@@ -3,7 +3,7 @@ import { slide as BurgerMenu } from "react-burger-menu";
 import styled from "styled-components";
 import media from "../utils/breakpoints";
 
-const BurgerWrapper = styled.div`
+const StyledBurgerMenu = styled.div`
   display: none;
   ${media.tablet`display: inherit;`};
   ${media.phone`display: inherit;`};
@@ -17,82 +17,190 @@ const BurgerWrapper = styled.div`
   z-index: 100;
   cursor: pointer;
   right: 0;
-`;
 
-const BurgerLines = styled.div`
-  background-color: #fff;
-  width: 100%;
-  height: 10px;
-  margin: 10px 0;
-  transition: all 450ms ease-in;
-`;
-
-const FullMenu = styled.div`
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  opacity: 0.5;
-  transition: all 400ms ease-in;
-  width: 100%;
-  height: 100%;
-  background: rgba(255, 113, 177, 1);
-  background: linear-gradient(
-    to right,
-    rgba(255, 113, 177, 1) 0%,
-    rgba(0, 255, 255, 1) 33%,
-    rgba(66, 250, 121, 1) 66%,
-    rgba(255, 243, 109, 1) 100%
-  );
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ff71b1', endColorstr='#fff36d', GradientType=1 );
-`;
-
-const ListItem = styled.li`
-  opacity: 0;
-  transform: translateY(2%);
-  transition: all 300ms ease-in;
-  cursor: pointer;
-  &:after {
-    content: "";
-    position: absolute;
-    width: 0%;
-    left: 0;
-    bottom: 0;
-    background-color: #fff;
-    transition: width 200ms ease-in;
+  /* Position and sizing of burger button */
+  .bm-burger-button {
+    position: fixed;
+    width: 5em;
+    height: 3em;
+    right: 0;
   }
-  &:hover:after {
-    width: 100%;
-  }    
+
+  /* Color/shape of burger icon bars */
+  .bm-burger-bars {
+    background: #373a47;
+  }
+
+  /* Position and sizing of clickable cross button */
+  .bm-cross-button {
+    height: 5em;
+    width: 6em;
+  }
+
+  /* Color/shape of close button cross */
+  .bm-cross {
+    background: #bdc3c7;
+  }
+
+  /* General sidebar styles */
+  .bm-menu {
+    top: 0;
+    background: linear-gradient(
+      to right,
+      rgba(255, 113, 177, 1) 0%,
+      rgba(0, 255, 255, 1) 33%,
+      rgba(66, 250, 121, 1) 66%,
+      rgba(255, 243, 109, 1) 100%
+    );
+    transition: all 400ms ease-in;
+    // padding: 2.5em 1.5em 0;
+    // font-size: 1.15em;
+  }
+
+  .bm-menu-wrap {
+    top: 0;
+  }
+
+  /* Morph shape necessary with bubble or elastic */
+  .bm-morph-shape {
+    fill: #373a47;
+  }
+
+  /* Wrapper for item list */
+  .bm-item-list {
+    display: grid;
+    justify-items: center;
+    align-items: center;
+  }
+
+  /* Individual item */
+  .bm-item {
+    display: inline-block;
+  }
+
+  /* Styling of overlay */
+  .bm-overlay {
+    background: rgba(0, 0, 0, 0.3);
+  }
+
+  // Styles of button
+
+  .hamburger {
+    padding: 15px 15px;
+    display: inline-block;
+    cursor: pointer;
+    transition-property: opacity, filter;
+    transition-duration: 0.15s;
+    transition-timing-function: linear;
+    font: inherit;
+    color: inherit;
+    text-transform: none;
+    background-color: transparent;
+    border: 0;
+    margin: 0;
+    overflow: visible;
+  }
+  .hamburger:hover {
+    opacity: 0.7;
+  }
+  .hamburger.is-active:hover {
+    opacity: 0.7;
+  }
+  .hamburger.is-active .hamburger-inner,
+  .hamburger.is-active .hamburger-inner::before,
+  .hamburger.is-active .hamburger-inner::after {
+    background-color: white;
+  }
+
+  .hamburger-box {
+    width: 40px;
+    height: 24px;
+    display: inline-block;
+    position: relative;
+  }
+
+  .hamburger-inner {
+    display: block;
+    top: 50%;
+    margin-top: -2px;
+  }
+  .hamburger-inner,
+  .hamburger-inner::before,
+  .hamburger-inner::after {
+    width: 40px;
+    height: 4px;
+    background-color: white;
+    border-radius: 4px;
+    position: absolute;
+    transition-property: transform;
+    transition-duration: 0.15s;
+    transition-timing-function: ease;
+  }
+  .hamburger-inner::before,
+  .hamburger-inner::after {
+    content: "";
+    display: block;
+  }
+  .hamburger-inner::before {
+    top: -10px;
+  }
+  .hamburger-inner::after {
+    bottom: -10px;
+  }
+  .hamburger--slider .hamburger-inner {
+    top: 2px;
+  }
+  .hamburger--slider .hamburger-inner::before {
+    top: 10px;
+    transition-property: transform, opacity;
+    transition-timing-function: ease;
+    transition-duration: 0.15s;
+  }
+  .hamburger--slider .hamburger-inner::after {
+    top: 20px;
+  }
+
+  .hamburger--slider.is-active .hamburger-inner {
+    transform: translate3d(0, 10px, 0) rotate(45deg);
+  }
+  .hamburger--slider.is-active .hamburger-inner::before {
+    transform: rotate(-45deg) translate3d(-5.71429px, -6px, 0);
+    opacity: 0;
+  }
+  .hamburger--slider.is-active .hamburger-inner::after {
+    transform: translate3d(0, -20px, 0) rotate(-90deg);
+  }
 `;
+
+
 
 export default class Menu extends React.Component {
   state = {
-    isMenuOpen: false,
+    menuOpen: false,
   }
-
-  handleOpen = () => {
-    this.setState(prevState =>({
-      isMenuOpen: !prevState.isMenuOpen,
-    }))
+  handleStateChange (state) {
+    this.setState({menuOpen: state.isOpen})  
   }
+  closeMenu () {
+    this.setState({menuOpen: false})
+  }
+  toggleMenu () {
+    this.setState({menuOpen: !this.state.menuOpen})
 
-  
-
-  
+  }
   render() {
-    console.log(this.state);
     return (
-      <div>
-      <BurgerWrapper onClick={this.handleOpen}>
-        <BurgerLines />
-        <BurgerLines />
-        <BurgerLines />
-      </BurgerWrapper>
-      {this.state.isMenuOpen && <FullMenu />}
-      </div>
-      
-
-
+      <StyledBurgerMenu>
+        <BurgerMenu isOpen={this.state.menuOpen}
+          onStateChange={(state) => this.handleStateChange(state)} customBurgerIcon={false} customCrossIcon={false} noOverlay right width="100%">
+          {this.props.children}
+        </BurgerMenu>
+        <button onClick={() => this.toggleMenu()} className={this.state.menuOpen ? "hamburger hamburger--slider is-active" : "hamburger hamburger--slider"} type="button">
+          <span className="hamburger-box">
+            <span className="hamburger-inner" />
+          </span>
+        </button>
+      </StyledBurgerMenu>
     );
   }
 }
