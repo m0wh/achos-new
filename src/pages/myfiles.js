6f6cf9c3571782815ea1a559e093243d64b797ee
Pol Milian <polcius@gmail.com>
2018-10-22 17:31:20 +1030

    got the data to display needs more testing with images

--- src/pages/myfiles.js
+++ src/pages/myfiles.js
@@ -1,25 +1,36 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Img from "gatsby-image";
 import Layout from "../components/layout";
 
 export default ({ data }) => {
   console.log(data);
+  const { frontmatter } = data.allMarkdownRemark.edges[0].node;
   return (
     <Layout>
-      <div>Hello world</div>
+      <h1>{frontmatter.name}</h1>
+      <h1>{frontmatter.introduction}</h1>
+      <Img fluid={frontmatter.bigimage1.childImageSharp.fluid} />
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile {
+    allMarkdownRemark {
       edges {
         node {
-          relativePath
-          prettySize
-          extension
-          birthTime(fromNow: true)
+          frontmatter {
+            name
+            introduction
+            bigimage1 {
+              childImageSharp {
+                fluid(maxWidth: 800) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
         }
       }
     }
