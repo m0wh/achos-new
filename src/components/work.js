6ff74926cb665705faaa89f3b221a1a197e5ee82
Pol Milian <polcius@gmail.com>
2018-10-29 15:21:43 +1030

    querying and displaying data, big image every five and responsive grid

--- src/components/work.js
+++ src/components/work.js
@@ -1,44 +1,41 @@
 import React from "react";
 import { Link } from "gatsby";
+import { Element } from "react-scroll";
 import styled from "styled-components";
+import Img from "gatsby-image";
+import media from "../utils/breakpoints";
 
-import MarianoPic from "../images/mariano.jpg";
-import AxpePic from "../images/axpe-01.jpg";
-import DadaPic from "../images/dadadada-00.jpg";
-import DesigndoesPic from "../images/designdoes.jpg";
-import SmallfilmsPic from "../images/smallfilms-00.jpg";
-
-/*
-Projects Schema:
-
-- Fecha
-- Introduccion
-- Creditos
-- Imagenes
-- Videos
-- Proyectos relacionados
-
-*/
-
-export const WorkGrid = styled.main`
+const WorkGrid = styled.main`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: auto;
+  ${media.phone`grid-template-columns: 1fr`};
 `;
 
-export const Project = styled.img`
-  max-width: 100%;
+const GridImage = styled(Img)`
   grid-column: ${props => (props.big ? "span 2" : null)};
 `;
-const Work = () => (
-  <WorkGrid>
-    <Project big src={MarianoPic} />
-    <Project src={AxpePic} />
-    <Project src={DadaPic} />
-    <Project src={DesigndoesPic} />
-    <Project src={MarianoPic} />
-    <Project big src={SmallfilmsPic} />
-  </WorkGrid>
-);
-
-export default Work;
+export default ({ projectEdges }) => {
+  console.log(projectEdges);
+  const everyFifth = [0, 5, 10, 15, 20, 25, 30];
+  // Want to display a grid where each 5th image is 100% wide. So we basically compare the projects' array index with our constant and if they match, we display a "big" image. Otherwise, just a small half-width image.
+  // Element is just a wrapper for react-scroll
+  return (
+    <Element name="work">
+      <WorkGrid>
+        {projectEdges.map(
+          project =>
+            everyFifth.includes(projectEdges.indexOf(project)) ? (
+              <GridImage
+                big
+                fluid={project.node.frontmatter.bigimage1.childImageSharp.fluid}
+              />
+            ) : (
+              <GridImage
+                fluid={project.node.frontmatter.bigimage1.childImageSharp.fluid}
+              />
+            )
+        )}
+      </WorkGrid>
+    </Element>
+  );
+};
