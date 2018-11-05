0317250b1b95290f7315da642a18fedb574c0408
Pol Milian <polcius@gmail.com>
2018-11-05 12:43:24 +1030

    randomColor function util finished

--- src/utils/randomColor.js
+++ src/utils/randomColor.js
@@ -1,4 +1,34 @@
-const randomColor = () => {
-  const colors = ["var(--pink)", "var(--cyan)", "var(--green)", "var(--yellow)"];
-  console.log(Math.random(4 * colors));
-};
+export default function randomColor() {
+  const colors = [
+    "var(--pink)",
+    "var(--cyan)",
+    "var(--green)",
+    "var(--yellow)"
+  ];
+
+  // "shuffle" from StackOverFlow :)
+  function shuffle(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    return array[getRandomInt(3)];
+  }
+
+  return shuffle(colors);
+}
