1411ca7688fc90158cc718f34ba7b2f8fa33910c
Pol Milian <polcius@gmail.com>
2018-11-07 16:03:37 +1030

    fontsizes outline

---
+++ src/utils/fontSizes.js
@@ -0,0 +1,18 @@
+// @media (min-width: 320px) {
+//   {
+//    font-size: calc(1.1rem + ((1vw - 0.2rem) * 2.4107));
+//  }
+// }
+// @media (min-width: 768px) {
+//   {
+//    font-size: 1.875rem;
+//  }
+// }
+
+/*
+1.875/1.1 = 1.7
+30px (normal) -> 1.875rem
+48px (big) -> 3rem
+
+
+*/
