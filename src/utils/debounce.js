f824c44e7aa38965456546677537507d0a2d8047
Pol Milian <polcius@gmail.com>
2018-11-18 21:04:30 +1030

    debounce for scroll events, not sure if this or throttle fn

---
+++ src/utils/debounce.js
@@ -0,0 +1,18 @@
+// from https://davidwalsh.name/javascript-debounce-function
+
+export default function debounce(func, wait, immediate) {
+  let timeout;
+  return function() {
+    const context = this;
+
+    const args = arguments;
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
