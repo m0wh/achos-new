70020fcd82d84d83ad3b01c69e6d0968cf88b633
Pol Milian <polcius@gmail.com>
2018-11-24 16:40:55 +0930

    basic behavior complleted

--- src/components/scrolltoclose.js
+++ src/components/scrolltoclose.js
@@ -2,49 +2,78 @@ import React from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
 import fontSizes from "../utils/fontSizes";
+import debounce from "../utils/debounce";
 
-// Hi ha un div on escolto "scroll" i a mesura que baixo augmenta l'opacitat d'un element child. Quan arribo a un cert nivell, funcio que em fa tornar enrere (goback).
 
 // http://qnimate.com/detecting-end-of-scrolling-in-html-element/
+
+
+// Hi ha un div d'una alcada determinada
+// Faig scroll a dins del div, no a window/document
+// A mesura que baixo, augmento opacitat de scroll to close
+// Quan arribo al final, funcio "goback"
+// Aquest scroll te una "resistencia" que es va fent mes gran com mes a prop del bottom
+
+// TODO: hide scrollbar (different code for each browser)
 
 const Wrapper = styled.div`
   display: grid;
   justify-items: center;
   align-items: start;
-  height: 80vh;
+  height: 500px;
+  overflow-y: scroll;
   ${fontSizes(1.875)}
 `;
 
-const Text = styled.p`
-  color: white;
+const Inner = styled.div`
+  height: 900px;
+  display: grid;
+  justify-items: center;
+  align-content: center;
 `;
 
-export default class ScrollToClose extends React.Component {
+const Text = styled.p`
+  color: rgba(255, 255, 255, ${props => props.opacity})
+`;
+
+class ScrollToClose extends React.Component {
 
   state = {
-    currentScrollHeight: '',
-  }
-  componentDidMount() {
-    // When this component mounts, begin listening for scroll changes
-    window.addEventListener('scroll', this.handleScroll);
-  }
+    opacity: 0.1,
+  };
 
-  componentWillUnmount() {
-    // If this component is unmounted, stop listening
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll = () => {
-    this.setState({ currentScrollHeight: window.scrollY });
-    console.log(this.state.currentScrollHeight);
-  }
+  handleScroll = (e) => {
+    const top = e.target.scrollTop === 0;
+    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+    console.log(e.target.scrollHeight, e.target.scrollTop);
+    this.setState({
+      opacity: this.state.opacity + 0.01,
+    })
+    if (top) {
+      this.setState({
+        opacity: 0.1,
+      })
+    }
+    if (bottom) { 
+      this.setState({
+        opacity: 1,
+        
+      })
+      typeof window !== `undefined` && window.history.back();
+      
+    }
+    
+  };
 
   render() {
-    const opacity = Math.max(100 / this.state.currentScrollHeight, 1);
     return (
-      <Wrapper>
-        <Text style={{opacity}}>Scroll to close</Text>
+      <Wrapper  onScroll={this.handleScroll}>
+        <Inner>
+          <Text opacity={this.state.opacity}>Scroll to close</Text>
+        </Inner>
       </Wrapper>
     );
   }
 }
+
+export default ScrollToClose;
