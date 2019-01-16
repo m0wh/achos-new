e1efe762ef84672e9dd40334320d526669e842c1
Pol Milian <polcius@gmail.com>
2019-01-16 18:31:03 +0930

    404 draft

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const NotFoundPage = () => (
   <>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404 Error: Studio not founded.</h1>
+
+<h4>Somos un laboratorio de creatividad, comunicación y diseño original en Barcelona. Nos aburre lo convencional y nos apasiona investigar, experimentar y realizar proyectos gráficos y publicitarios. Estamos empezando y creemos que sería un error del 404, invertir ahora tiempo en nuestra web, cuando estamos creando soluciones para The Ting Tings, Singaz Barcelona y Canvas Ibiza entre otros... en el que combinamos medios alternativos con un pensamiento lateral. Si quieres saber más sobre nosotros, te esperamos acho!</h4> 
+
+<h4>achos! · Vigatans 11 Local 2, 08003 Barcelona · Tel. 935 016 671 · hi@achos.es · www.achos.es</h4>
   </>
 );
 
