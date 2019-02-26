2216f1286a0f4c541a84eb617c874d77277ec741
Pol Milian <polcius@gmail.com>
2019-02-26 18:05:58 +0930

    removed logo animation

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,42 +1,38 @@
-import React from "react";
-import { Link } from "gatsby";
-import styled from "styled-components";
-import Fade from "react-reveal/Fade";
-import fontSizes from "../utils/fontSizes";
+import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+import fontSizes from '../utils/fontSizes'
 
-import SoundGif from "./soundgif";
+import SoundGif from './soundgif'
 
-import Sound from "../images/barcelona.mp3";
-import GIF from "../images/gifs/5sectour.gif";
+import Sound from '../images/barcelona.mp3'
+import GIF from '../images/gifs/5sectour.gif'
 
 const LogoWrapper = styled.div`
   // z-index: 1000;
-`;
+`
 
 const LogoText = styled.h2`
-  ${fontSizes(5)};
+  ${ fontSizes(5) };
   letter-spacing: 3px;
   line-height: 100px;
-`;
+`
 
-
-const locationArray = ["/", "/contact", "/about"];
+const locationArray = ['/', '/contact', '/about']
 
 const Logo = () => (
   <LogoWrapper>
     <SoundGif sound={Sound} gif={GIF}>
       <Link to="/">
-        <Fade duration={3000} top cascade>
-          <LogoText>
-            {typeof location !== `undefined` &&
+        <LogoText>
+          {typeof location !== `undefined` &&
             locationArray.includes(location.pathname)
-              ? "achos!"
-              : "achos&"}
-          </LogoText>
-        </Fade>
+            ? 'achos!'
+            : 'achos&'}
+        </LogoText>
       </Link>
     </SoundGif>
   </LogoWrapper>
-);
+)
 
-export default Logo;
+export default Logo
