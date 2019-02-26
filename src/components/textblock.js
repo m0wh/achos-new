fd31c4b8ade3e4316fec3e375ac242b271d1ba9a
Pol Milian <polcius@gmail.com>
2019-02-26 18:17:49 +0930

    no fade on centered text

--- src/components/textblock.js
+++ src/components/textblock.js
@@ -1,43 +1,43 @@
-import React from "react";
-import styled from "styled-components";
-import Fade from "react-reveal/Fade";
-import fontSizes from "../utils/fontSizes";
-import media from "../utils/breakpoints";
+import React from 'react'
+import styled from 'styled-components'
+import Fade from 'react-reveal/Fade'
+import fontSizes from '../utils/fontSizes'
+import media from '../utils/breakpoints'
 
 export const Wrapper = styled.article`
   display: grid;
   grid-template-columns: 1fr 4fr;
   grid-gap: 1vw;
   align-items: baseline;
-  ${fontSizes(1.875)}
+  ${ fontSizes(1.875) }
   margin: 0 3.5625vw;
-  padding: ${props => props.padding};
-`;
+  padding: ${ props => props.padding };
+`
 
 const TextLeft = styled.p`
   grid-column-start: 1;
   grid-column-end: 2;
-  color: ${props => props.color};
-  font-size: ${props => props.sizeLeft};
-`;
+  color: ${ props => props.color };
+  font-size: ${ props => props.sizeLeft };
+`
 const TextRight = styled.p`
   grid-column-start: 2;
   grid-column-end: 3;
-  ${media.phone`
+  ${ media.phone`
     grid-column-start: 1;
     grid-column-end: -1;
-  `}
+  ` }
 
   color: inherit;
-  font-size: ${props => props.sizeRight};
-`;
+  font-size: ${ props => props.sizeRight };
+`
 const TextCenter = styled.p`
   grid-column: span 2;
   justify-items: center;
   text-align: center;
-  font-size: ${props => props.sizeCenter};
-  color: ${props => props.color};
-`;
+  font-size: ${ props => props.sizeCenter };
+  color: ${ props => props.color };
+`
 const TextBlock = ({
   padding,
   sizeLeft,
@@ -57,11 +57,9 @@ const TextBlock = ({
       <Fade duration={3000}>{textRight}</Fade>
     </TextRight>
     <TextCenter sizeCenter={sizeCenter} color={colorCenter}>
-      <Fade duration={2000} delay={1500}>
-        {textCenter}
-      </Fade>
+      {textCenter}
     </TextCenter>
   </Wrapper>
-);
+)
 
-export default TextBlock;
+export default TextBlock
