d9cf1c51100dceb74fcc769cad8257aefd58159c
Pol Milian <polcius@gmail.com>
2019-02-27 20:47:04 +0930

    decrease opacity on not hovered text and small animation

--- src/components/soundgif.js
+++ src/components/soundgif.js
@@ -1,24 +1,25 @@
-import React, { Component } from "react";
-import styled, { createGlobalStyle } from "styled-components";
-
+import React, { Component } from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
 
 const BackgroundGif = createGlobalStyle`
   body {
-    background: url("${props => props.gif}") no-repeat center center fixed; 
+    background: url("${ props => props.gif }") no-repeat center center fixed; 
     background-size: cover;
     div:not(:hover) {
       opacity: 0;
+      transition: all 0.5s;
     }
     ul:not(:hover) {
-      opacity: 0;
+      opacity: 0
+      transition: all 0.5s;
     }
     span:not(:hover) {
-      opacity: 0;
+      opacity: 0.3;
+      transition: all 0.5s;
     }
     
   }
-`;
-
+`
 
 class SoundGIF extends Component {
   state = {
@@ -28,46 +29,42 @@ class SoundGIF extends Component {
 
   myRef = React.createRef();
 
-
   startIt = () => {
-    this.myRef.current.play();
+    this.myRef.current.play()
     this.setState({
-          isAudioPlaying: true,
-          isGifShowing: true
-        });
+      isAudioPlaying: true,
+      isGifShowing: true
+    })
   }
 
   stopIt = () => {
     this.setState({
       isAudioPlaying: false,
       isGifShowing: false
-    });
-    this.myRef.current.pause();
-    this.myRef.current.currentTime = 0;
-   
+    })
+    this.myRef.current.pause()
+    this.myRef.current.currentTime = 0
   }
 
-  render() {
+  render () {
     const isGifShowing = this.state.isGifShowing
     const { children } = this.props
-
 
     return (
       <>
         <span onMouseOver={this.startIt} onMouseOut={this.stopIt} onClick={this.stopIt}>
           {children}
         </span>
-        
+
         <audio loop preload="auto" ref={this.myRef} >
           <source src={this.props.sound} type="audio/mpeg"></source>
         </audio>
 
         { isGifShowing && <BackgroundGif gif={this.props.gif} />}
       </>
-        
-    );
+
+    )
   }
 }
 
-export default SoundGIF;
-
+export default SoundGIF
