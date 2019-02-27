db89f30dd5ab9936d0f0ffbae2ac2777a7109035
Pol Milian <polcius@gmail.com>
2019-02-27 20:23:33 +0930

    using props to differentiate between big and small grid items

--- src/components/work.js
+++ src/components/work.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Element } from 'react-scroll'
 import styled from 'styled-components'
-// import Fade from "react-reveal/Fade";
-import Fade from 'react-reveal/Fade'
+
 import media from '../utils/breakpoints'
 import GridImage from './gridimage'
 import randomColor from '../utils/randomColor'
@@ -30,39 +29,25 @@ const WorkGrid = styled.main`
 `
 
 export default ({ projectEdges }) => {
-  const everyFifth = [0, 5, 10, 15, 20, 25, 30]
-  // Want to display a grid where each 5th image is 100% wide. So we basically compare the projects' array index with our constant and if they match, we display a "big" image. Otherwise, just a small half-width image.
-  // Hovering the big images also plays a random moan sound.
   // Element is just a wrapper for react-scroll
   return (
     <Element name="work">
       <WorkGrid>
-        <Fade duration={500} delay={400}>
-          {projectEdges.map(project =>
-            everyFifth.includes(projectEdges.indexOf(project)) ? (
-              <GridImage
-                color={randomColor()}
-                key={project.node.frontmatter.name}
-                big
-                fluid={project.node.frontmatter.bigimage1.childImageSharp.fluid}
-                name={project.node.frontmatter.name}
-                category={project.node.frontmatter.category}
-                link={project.node.fields.slug}
-                sound={randomMoan()}
-              />
-            ) : (
-              <GridImage
-                color={randomColor()}
-                key={project.node.frontmatter.name}
-                fluid={project.node.frontmatter.bigimage1.childImageSharp.fluid}
-                name={project.node.frontmatter.name}
-                category={project.node.frontmatter.category}
-                link={project.node.fields.slug}
-                sound={TickSound}
-              />
-            )
-          )}
-        </Fade>
+        {projectEdges.map(project =>
+          (
+            <GridImage
+              color={randomColor()}
+              key={project.node.frontmatter.name}
+              big={project.node.frontmatter.isbig}
+              fluid={project.node.frontmatter.bigimage1.childImageSharp.fluid}
+              name={project.node.frontmatter.name}
+              category={project.node.frontmatter.category}
+              link={project.node.fields.slug}
+              sound={project.node.frontmatter.isbig ? randomMoan() : TickSound}
+            />
+
+          )
+        )}
       </WorkGrid>
     </Element>
   )
