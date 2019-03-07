94197b9c4eb14a92f31f2e1118250a94fca047cf
Pol Milian <polcius@gmail.com>
2019-03-07 13:52:50 +0930

    replaced hardcoded token for .env variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,24 @@
+require('dotenv').config({
+  path: `.env.${ process.env.NODE_ENV }`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: "achos!",
-    titleTemplate: "achos&%s!",
+    title: 'achos!',
+    titleTemplate: 'achos&%s!',
     description:
-      "achos! attention-grabbing, panty-dropping communication and interactive design agency based in Barcelona and Silicon Bali.",
-    url: "https://www.doe.com", // No trailing slash allowed!
-    image: "src/images/achoslogo.png", // Path to your image you placed in the 'static' folder
-    twitterUsername: "@achosagency"
+      'achos! attention-grabbing, panty-dropping communication and interactive design agency based in Barcelona and Silicon Bali.',
+    url: 'https://www.doe.com', // No trailing slash allowed!
+    image: 'src/images/achoslogo.png', // Path to your image you placed in the 'static' folder
+    twitterUsername: '@achosagency'
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-source-contentful`,
       options: {
-        name: `projects`,
-        path: `${__dirname}/src/projects/`
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `locationimages`,
-        path: `${__dirname}/src/images/locations`
-      }
+        spaceId: `7q0ttflu0eox`,
+        accessToken: `${ process.env.ACCESS_TOKEN }`,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -31,16 +28,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "achos!",
-        short_name: "achos!",
-        start_url: "/",
-        background_color: "#333",
-        theme_color: "#333",
-        display: "minimal-ui",
-        icon: "src/images/achoslogo.png" // This path is relative to the root of the site.
+        name: 'achos!',
+        short_name: 'achos!',
+        start_url: '/',
+        background_color: '#333',
+        theme_color: '#333',
+        display: 'minimal-ui',
+        icon: 'src/images/achoslogo.png' // This path is relative to the root of the site.
       }
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`
   ]
-};
+}
