4198c52ccb4cca7fe4b140450bf9ebc5ab79afd8
Pol Milian <polcius@gmail.com>
2019-03-20 15:23:17 +1030

    name refactor and debouncing. needs a lot more tweaking!

--- src/components/resizer.js
+++ src/components/resizer.js
@@ -1,49 +1,48 @@
-import React, { Component } from "react";
-import styled from "styled-components";
-import DidgeridooImage from "../images/didgeridoo.jpg"
-import DidgeridooSound from "../images/didgeridoosound.mp3"
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import _ from 'lodash'
+import DidgeridooImage from '../images/didgeridoo.jpg'
+import DidgeridooSound from '../images/didgeridoosound.mp3'
 
 const Wrapper = styled.div`
-  background: url("${DidgeridooImage}") no-repeat center center fixed; 
+  background: url("${ DidgeridooImage }") no-repeat center center fixed; 
   background-size: cover;
   top: 0;
   left: 0;
   z-index: 3;
   width: 100vw;
   height: 100vh;
-`;
-
+`
+// https://codesandbox.io/s/m76o28w4k9?from-embed
 
 class Resizer extends Component {
-
   state = {
-    resizing: false
+    isResizing: false
   }
 
-  componentDidMount() {
-    window.addEventListener("resize", this.onResize)
+  componentDidMount () {
+    window.addEventListener('resize', _.debounce(this.onResize, 20))
   }
 
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.onResize)
+  componentWillUnmount () {
+    window.removeEventListener('resize', _.debounce(this.onResize, 20))
   }
 
   onResize = () => {
     this.setState({
-      resizing: true
-    });
-    setTimeout(() => this.setState({resizing: false}), 1000);
+      isResizing: true
+    })
+    setTimeout(() => this.setState({ isResizing: false }), 500)
   }
 
-
-  render() {
+  render () {
     return (
       <div onResize={this.onResize}>
-        {this.state.resizing && <Wrapper />}
-        {this.state.resizing && <audio autoPlay src={DidgeridooSound}></audio>}
+        {this.state.isResizing && <Wrapper />}
+        {this.state.isResizing && <audio autoPlay src={DidgeridooSound}></audio>}
       </div>
-    );
+    )
   }
 }
 
-export default Resizer;
+export default Resizer
