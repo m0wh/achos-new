51e7645d7b731a7bfe626557bc62539a3afc06e2
Pol Milian <polcius@gmail.com>
2019-04-08 13:46:51 +0800

    updates + lint

--- src/utils/randomColor.js
+++ src/utils/randomColor.js
@@ -1,34 +1,34 @@
-export default function randomColor() {
+export default function randomColor () {
   const colors = [
-    "var(--pink)",
-    "var(--cyan)",
-    "var(--green)",
-    "var(--yellow)"
-  ];
+    'var(--pink)',
+    'var(--cyan)',
+    'var(--green)',
+    'var(--yellow)'
+  ]
 
   // "shuffle" from StackOverFlow :)
-  function shuffle(array) {
-    let currentIndex = array.length;
-    let temporaryValue;
-    let randomIndex;
+  function shuffle (array) {
+    let currentIndex = array.length
+    let temporaryValue
+    let randomIndex
 
     // While there remain elements to shuffle...
     while (currentIndex !== 0) {
       // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex -= 1
 
       // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+      temporaryValue = array[currentIndex]
+      array[currentIndex] = array[randomIndex]
+      array[randomIndex] = temporaryValue
     }
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
+    function getRandomInt (max) {
+      return Math.floor(Math.random() * Math.floor(max))
     }
 
-    return array[getRandomInt(3)];
+    return array[getRandomInt(3)]
   }
 
-  return shuffle(colors);
+  return shuffle(colors)
 }
