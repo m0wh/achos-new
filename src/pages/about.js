7e19b2bedc780be4f800c4ea8e85b272490729a5
Pol Milian <polcius@gmail.com>
2019-04-11 19:27:31 +0800

    sourcing list from contentful

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import styled from 'styled-components'
 
 import TextBlock from '../components/textblock'
@@ -30,113 +30,6 @@ const aboutText = [
 const hotBranding =
   'art direction / bondage / events / graphic design / illustration / digital marketing / sex / print /  editorial / naming / messaging / tone of voice / strategy / positioning / latex / packaging design / porn / social media / film / WOW factor! / creative team'
 
-const blaBoomBling = [
-  [
-    { name: "EVA '17", link: 'http://evafest.com/' },
-    {
-      name: "Pecha Kucha Den Haag '17",
-      link: 'http://www.pechakuchadenhaag.nl'
-    },
-    {
-      name: "ADCE Potencial '17",
-      link:
-        'http://www.adceurope.org/news/detail/id/140/adce-high-potentials-comes-to-munich-in-2017'
-    },
-    { name: "OFFF Barcelona '16", link: 'http://www.offf.ws' },
-    { name: "ENED '16", link: 'https://ened.pt' },
-    { name: "ADCE '15", link: 'http://europeancreativityfestival.com' },
-    { name: "Idep Barcelona '15", link: 'http://www.idep.es' },
-    { name: "FIU Barcelona '15", link: 'http://www.fiubcn.com' },
-    {
-      name: "Student Laus Tour '14",
-      link: 'http://www.fad.cat/adg/blog/2014/12/01/students-laus-tour/'
-    },
-    { name: "FIU Barcelona '14", link: 'http://www.fiubcn.com' },
-    { name: "UAB '14", link: 'http://www.fiubcn.com' }
-  ],
-  [
-    {
-      name: "Design Kids '17",
-      link: 'http://thedesignkids.org/interviews/achos/'
-    },
-    {
-      name: "TIA '17",
-      link:
-        'http://www.topinteractiveagencies.com/digital/agency/profiles/mixing-work-pleasure-search-creativity/'
-    },
-    {
-      name: "Charlie Magazine '15",
-      link: 'http://charliemag.be/wereld/barcelona1/'
-    },
-    {
-      name: "el Periodico '15",
-      link:
-        'http://www.elperiodico.com/es/noticias/sociedad/espai-born-compartir-espacio-los-proyectos-trabajo-4186661'
-    },
-    {
-      name: "Graffica '15",
-      link: 'http://graffica.info/achos-robo-sagmeister-walsh/'
-    },
-    {
-      name: "Creative Review '15",
-      link:
-        'http://www.creativereview.co.uk/cr-blog/2015/april/sagmeister-walsh-and-the-robbery-that-never-was/'
-    },
-    {
-      name: "BOOOOOOOM '15",
-      link:
-        'http://www.booooooom.com/2015/04/21/how-to-get-noticed-steal-work-from-sagmeister-walsh/'
-    },
-    { name: "Murcia Visual '14", link: 'http://www.murciavisual.com/?p=8170' },
-    { name: "Idep Barcelona '14", link: 'http://www.idep.es' },
-    {
-      name: "Small Job '14",
-      link:
-        'http://blog.asmalljob.com/?utm_source=rss&utm_medium=rss&utm_campaign=como-partir-un-laus'
-    }
-  ],
-  [
-    {
-      name: "Developer Award '18",
-      link: 'https://www.awwwards.com/sites/mariano-pascual'
-    },
-    {
-      name: "Awwwards SOTD '18",
-      link: 'https://www.awwwards.com/sites/mariano-pascual'
-    },
-    {
-      name: "Brutalist Website '17",
-      link: 'http://brutalistwebsites.com/dictionaryofonlinebehavior.com/'
-    },
-    {
-      name: "Honors Awwwards '17",
-      link: 'https://www.awwwards.com/sites/lazy-eyes'
-    },
-    {
-      name: "Brutalist Website '17",
-      link: 'http://brutalistwebsites.com/lazyeyes.cool/'
-    },
-    {
-      name: "Honors Awwwards '17",
-      link: 'https://www.awwwards.com/sites/codea-studio'
-    },
-    {
-      name: "Brutalist Website '17",
-      link: 'http://brutalistwebsites.com/codeastudio.com/'
-    },
-    {
-      name: "Honors Awwwards '17",
-      link: 'https://www.awwwards.com/sites/collabwith-jen-lu'
-    },
-    {
-      name: "Honors Awwwards '17",
-      link: 'https://www.awwwards.com/sites/smash-post-production-company'
-    },
-    { name: "Laus '15", link: '' },
-    { name: "Laus '14", link: '' }
-  ]
-]
-
 const ProjectWrapper = styled.main`
   background-color: var(--lightblack);
   color: var(--lightgrey);
@@ -154,7 +47,7 @@ const VideoWrapper = styled.div`
 const ShowsContainer = styled.section`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-column-gap: 3vw;
+  grid  -gap: 3vw;
   justify-items: center;
   align-items: baseline;
   text-align: center;
@@ -172,7 +65,7 @@ const CreditsList = styled.ul`
   justify-items: center;
 `
 
-export default () => (
+export default ({ data }) => (
   <>
     <ProjectWrapper>
       <VideoWrapper style={{ marginTop: 0, marginBottom: '6rem' }}>
@@ -249,13 +142,13 @@ export default () => (
       <ShowsContainer>
         <ul>
           <Fade duration={1000}>
-            <ul style={{ color: 'var(--green)' }}>Bla Bla</ul>
+            <ul style={{ color: 'var(--cyan)' }}>{data.allContentfulListaAbout.edges[2].node.titulo}</ul>
           </Fade>
           <Fade cascade duration={2000}>
-            {blaBoomBling[0].map(item => (
+            {data.allContentfulListaAbout.edges[2].node.enlaces.map(item => (
               <li key={item.id}>
-                <a href={item.link} target="_blank" rel="noopener noreferrer">
-                  {item.name}
+                <a href={item.enlace} target="_blank" rel="noopener noreferrer">
+                  {item.texto}
                 </a>
               </li>
             ))}
@@ -263,13 +156,13 @@ export default () => (
         </ul>
         <ul>
           <Fade duration={1000}>
-            <ul style={{ color: 'var(--cyan)' }}>Boom Boom</ul>
+            <ul style={{ color: 'var(--yellow)' }}>{data.allContentfulListaAbout.edges[1].node.titulo}</ul>
           </Fade>
           <Fade cascade duration={2000}>
-            {blaBoomBling[1].map(item => (
+            {data.allContentfulListaAbout.edges[1].node.enlaces.map(item => (
               <li key={item.id}>
-                <a href={item.link} target="_blank" rel="noopener noreferrer">
-                  {item.name}
+                <a href={item.enlace} target="_blank" rel="noopener noreferrer">
+                  {item.texto}
                 </a>
               </li>
             ))}
@@ -277,13 +170,13 @@ export default () => (
         </ul>
         <ul>
           <Fade duration={1000}>
-            <ul style={{ color: 'var(--yellow)' }}>Bling Bling</ul>
+            <ul style={{ color: 'var(--pink)' }}>{data.allContentfulListaAbout.edges[0].node.titulo}</ul>
           </Fade>
           <Fade cascade duration={2000}>
-            {blaBoomBling[2].map(item => (
+            {data.allContentfulListaAbout.edges[0].node.enlaces.map(item => (
               <li key={item.id}>
-                <a href={item.link} target="_blank" rel="noopener noreferrer">
-                  {item.name}
+                <a href={item.enlace} target="_blank" rel="noopener noreferrer">
+                  {item.texto}
                 </a>
               </li>
             ))}
@@ -293,3 +186,19 @@ export default () => (
     </ProjectWrapper>
   </>
 )
+
+export const query = graphql`
+  query AboutPageQuery {
+    allContentfulListaAbout {
+      edges {
+        node {
+          titulo
+          enlaces {
+            texto
+            enlace
+          }
+        }
+      }
+    }
+  }  
+`
