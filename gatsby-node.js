0ee810feb5beb8b7a61ed0deda89c487c27912c2
Pol Milian <polcius@gmail.com>
2019-04-21 20:35:58 +0930

    for mdx

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,58 +1,86 @@
-const _ = require(`lodash`)
-const path = require(`path`)
-const slash = require(`slash`)
+const _ = require('lodash')
 
-// Implement the Gatsby API “createPages”. This is
-// called after the Gatsby bootstrap is finished so you have
-// access to any information necessary to programmatically
-// create pages.
-exports.createPages = ({ graphql, actions }) => {
+// graphql function doesn't throw an error so we have to check to check for the result.errors to throw manually
+const wrapper = promise =>
+  promise.then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+    return result
+  })
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  let slug
+  // Only use MDX nodes
+  if (node.internal.type === 'Mdx') {
+    const fileNode = getNode(node.parent)
+    // If the frontmatter contains a "slug", use it
+    if (
+      Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
+      Object.prototype.hasOwnProperty.call(node.frontmatter, 'slug')
+    ) {
+      slug = `/${ _.kebabCase(node.frontmatter.slug) }`
+    }
+    // Otherwise use the title for the slug
+    if (
+      Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
+      Object.prototype.hasOwnProperty.call(node.frontmatter, 'title')
+    ) {
+      slug = `/${ _.kebabCase(node.frontmatter.title) }`
+    }
+    createNodeField({ node, name: 'slug', value: slug })
+    // Adds the name of "gatsby-source-filesystem" as field (in this case "projects" or "pages")
+    createNodeField({ node, name: 'sourceInstanceName', value: fileNode.sourceInstanceName })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  // The “graphql” function allows us to run arbitrary
-  // queries against the local Contentful graphql schema. Think of
-  // it like the site has a built-in database constructed
-  // from the fetched data that you can run queries against.
-  return graphql(
-    `
+
+  // Our templates for projects and files inside /pages/*.mdx
+  const projectPage = require.resolve('./src/templates/projectpage.js')
+
+  const result = await wrapper(
+    graphql(`
       {
-        allContentfulProyecto(
-          sort: { fields: [orden], order: DESC }
-          limit: 1000
-        ) {
+        projects: allMdx(filter: { fields: { sourceInstanceName: { eq: "projects" } } }) {
           edges {
             node {
-              slug
+              fields {
+                slug
+              }
             }
           }
         }
       }
-    `
+    `)
   )
-    .then(result => {
-      if (result.errors) {
-        throw result.errors
-      }
 
-      // Create Product pages
-      const proyectoTemplate = path.resolve(`./src/templates/projectpage.js`)
-      // We want to create a detailed page for each
-      // product node. We'll just use the Contentful id for the slug.
-      _.each(result.data.allContentfulProyecto.edges, edge => {
-        // Gatsby uses Redux to manage its internal state.
-        // Plugins and sites can use functions like "createPage"
-        // to interact with Gatsby.
-        createPage({
-          // Each page is required to have a `path` as well
-          // as a template component. The `context` is
-          // optional but is often necessary so the template
-          // can query data specific to each page.
-          path: `/${ edge.node.slug }/`,
-          component: slash(proyectoTemplate),
-          context: {
-            slug: edge.node.slug
-          },
-
-        })
-      })
+  result.data.projects.edges.forEach(edge => {
+    createPage({
+      path: edge.node.fields.slug,
+      component: projectPage,
+      context: {
+        // Pass "slug" through context so we can reference it in our query like "$slug: String!"
+        slug: edge.node.fields.slug,
+      },
     })
+  })
 }
+
+// Necessary changes to get gatsby-mdx and Cypress working
+// exports.onCreateWebpackConfig = ({ stage, actions, loaders, getConfig }) => {
+//   const config = getConfig()
+
+//   config.module.rules = [
+//     ...config.module.rules.filter(rule => String(rule.test) !== String(/\.jsx?$/)),
+//     {
+//       ...loaders.js(),
+//       test: /\.jsx?$/,
+//       exclude: modulePath => /node_modules/.test(modulePath) && !/node_modules\/gatsby-mdx/.test(modulePath),
+//     },
+//   ]
+
+//   actions.replaceWebpackConfig(config)
+// }
