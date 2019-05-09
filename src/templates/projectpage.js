ca3a8b20e6fc7c49baf11ae5743455da95218d42
Pol Milian <polcius@gmail.com>
2019-05-09 13:39:52 +0930

    no fade on images

--- src/templates/projectpage.js
+++ src/templates/projectpage.js
@@ -53,63 +53,53 @@ export default ({ data }) => {
           sizeCenter={fontSizes(5)}
           colorCenter="white"
         />
-        <Fade duration={500}>
-          <div style={{ maxWidth: '100%' }}>
-            <ProjectImg fluid={node.imagenPortada.fluid} />
-          </div>
-        </Fade>
+        <div style={{ maxWidth: '100%' }}>
+          <ProjectImg fluid={node.imagenPortada.fluid} />
+        </div>
         <TextBlock
           colorLeft={randomColor()}
           textLeft={node.bloquesDeTexto[0].titulo}
           textRight={node.bloquesDeTexto[0].childContentfulBloqueDeTextoBodyTextNode.childMarkdownRemark.rawMarkdownBody}
           padding="4vw 0"
         />
-        <Fade duration={500}>
-          {node.imagenes[0] && (<ProjectImg small={node.imagenes[0].esPequena} fluid={node.imagenes[0].imagen.fluid} />)}
+        {node.imagenes[0] && (<ProjectImg small={node.imagenes[0].esPequena} fluid={node.imagenes[0].imagen.fluid} />)}
 
-          {/* {node.gifsvideos[0] && (<video
+        {/* {node.gifsvideos[0] && (<video
             loop
             controls
             style={{ width: '100%' }}
             src={node.gifsvideos[0].archivo.file.url}
           />)} */}
-        </Fade>
         <TextBlock
           colorLeft={randomColor()}
           textLeft={node.bloquesDeTexto[1].titulo}
           textRight={node.bloquesDeTexto[1].childContentfulBloqueDeTextoBodyTextNode.childMarkdownRemark.rawMarkdownBody}
           padding="4vw 0"
         />
-        <Fade cascade duration={500}>
-          <div>
-            {node.imagenes[1] && (<ProjectImg small={node.imagenes[1].esPequena} fluid={node.imagenes[1].imagen.fluid} />)}
-            {node.imagenes[2] && (<ProjectImg small={node.imagenes[2].esPequena} fluid={node.imagenes[2].imagen.fluid} />)}
-          </div>
-        </Fade>
+        <div>
+          {node.imagenes[1] && (<ProjectImg small={node.imagenes[1].esPequena} fluid={node.imagenes[1].imagen.fluid} />)}
+          {node.imagenes[2] && (<ProjectImg small={node.imagenes[2].esPequena} fluid={node.imagenes[2].imagen.fluid} />)}
+        </div>
         {node.bloquesDeTexto[2] && (<TextBlock
           colorLeft={randomColor()}
           textLeft={node.bloquesDeTexto[2].titulo}
           textRight={node.bloquesDeTexto[2].childContentfulBloqueDeTextoBodyTextNode.childMarkdownRemark.rawMarkdownBody}
           padding="4vw 0"
         />)}
-        <Fade cascade duration={500}>
-          <div>
-            {node.imagenes[3] && (<ProjectImg small={node.imagenes[3].esPequena} fluid={node.imagenes[3].imagen.fluid} />)}
-            {node.imagenes[4] && (<ProjectImg small={node.imagenes[4].esPequena} fluid={node.imagenes[4].imagen.fluid} />)}
-          </div>
-        </Fade>
+        <div>
+          {node.imagenes[3] && (<ProjectImg small={node.imagenes[3].esPequena} fluid={node.imagenes[3].imagen.fluid} />)}
+          {node.imagenes[4] && (<ProjectImg small={node.imagenes[4].esPequena} fluid={node.imagenes[4].imagen.fluid} />)}
+        </div>
         {node.bloquesDeTexto[3] && (<TextBlock
           colorLeft={randomColor()}
           textLeft={node.bloquesDeTexto[3].titulo}
           textRight={node.bloquesDeTexto[3].childContentfulBloqueDeTextoBodyTextNode.childMarkdownRemark.rawMarkdownBody}
           padding="4vw 0"
         />)}
-        <Fade cascade duration={500}>
-          <div>
-            {node.imagenes[5] && (<ProjectImg small={node.imagenes[5].esPequena} fluid={node.imagenes[5].imagen.fluid} />)}
-            {node.imagenes[6] && (<ProjectImg small={node.imagenes[6].esPequena} fluid={node.imagenes[6].imagen.fluid} />)}
-          </div>
-        </Fade>
+        <div>
+          {node.imagenes[5] && (<ProjectImg small={node.imagenes[5].esPequena} fluid={node.imagenes[5].imagen.fluid} />)}
+          {node.imagenes[6] && (<ProjectImg small={node.imagenes[6].esPequena} fluid={node.imagenes[6].imagen.fluid} />)}
+        </div>
 
         <Fade cascade duration={2000}>
           <Wrapper style={{ padding: '4vw 0' }}>
