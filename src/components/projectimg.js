937f53e8c271f2a300ab14a293c0d5815f91c161
Pol Milian <polcius@gmail.com>
2019-05-12 17:16:55 +0930

    images full width on mobile

--- src/components/projectimg.js
+++ src/components/projectimg.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components'
 import Img from 'gatsby-image'
+import media from '../utils/breakpoints'
 
 const ProjectImg = styled(Img)`
 width: ${ props => props.small ? '50%' : '' };
 display: ${ props => props.small ? 'inline-block' : '' };
+${ media.phone`width: 100%;
+  display: block;
+` }
+
 `
 export default ProjectImg
