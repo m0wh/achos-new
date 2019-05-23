d8ba60d831ac6c2b320459de057bc94c360145ce
Pol Milian <polcius@gmail.com>
2019-05-23 18:49:33 +0200

    more range

--- src/components/shootingrangeelement.js
+++ src/components/shootingrangeelement.js
@@ -54,8 +54,8 @@ const ShootingRangeElement = ({ image }) => {
     <Container onClick={handleClick} style={{
       transform: x
         .interpolate({
-          range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-          output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
+          range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+          output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
         })
         .interpolate(x => `scale(${ x })`)
     }} image={image} >
