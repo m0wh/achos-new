5661a140dcccce799a38e8336127eda5e6c5ca79
Pol Milian <polcius@gmail.com>
2019-05-27 20:31:22 +0200

    unused code

--- src/styles/poses.js
+++ src/styles/poses.js
@@ -19,33 +19,3 @@ export const pageFade = {
     filter: 'blur(100px)'
   }
 }
-
-export const staggerChildren = {
-  enter: { staggerChildren: 50 }
-}
-
-export const delayChildren = {
-  enter: { delayChildren: transitionDelay }
-}
-
-export const slideUp = {
-  initial: { y: '110%' },
-  enter: { y: 0, transition: { duration: transitionDuration } }
-}
-
-export const appear = {
-  initial: { opacity: 0, y: 10 },
-  enter: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: transitionDuration }
-  }
-}
-
-export const fade = {
-  initial: { opacity: 0 },
-  enter: {
-    opacity: 1,
-    transition: { duration: transitionDuration }
-  }
-}
