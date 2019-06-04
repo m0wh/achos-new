635a3478df5b8083bcdea96d6698898667951947
Pol Milian <polcius@gmail.com>
2019-06-04 18:45:56 +0200

    adjusted styles, using gatsby's navigate

--- src/components/scrolltoclose.js
+++ src/components/scrolltoclose.js
@@ -2,35 +2,27 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import fontSizes from '../utils/fontSizes'
 import { useScrollPercentage } from 'react-scroll-percentage'
+import { navigate } from 'gatsby'
 
 const Wrapper = styled.div`
   display: grid;
   justify-items: center;
   align-items: center;
-  height: 60vh;
-  padding: 10rem 0 20rem 0;
-  margin-bottom: 20rem;
+  height: 30vh;
+  padding: 0 20rem;
+  margin-bottom: 50rem;
   color: white;
   ${ fontSizes(1.875) }
 `
 
 const ScrollToClose = () => {
-  const takeMeBack = (function () {
-    let executed = false
-    return function () {
-      if (!executed) {
-        executed = true
-        window.history.back()
-      }
-    }
-  })()
   const [ref, percentage] = useScrollPercentage({
     threshold: 0.25,
   })
   return (
     <Wrapper>
       <p ref={ref} style={{ opacity: `${ percentage.toFixed(2) }` }}>Scroll to close</p>
-      {percentage === 1 ? takeMeBack() : null}
+      {percentage === 1 ? navigate('/', { replace: true }) : null}
     </Wrapper>
   )
 }
